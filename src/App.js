a9c3b85d901617235f8768cb9160aa1015598f63
Lewis Smith <66628534+PKThunda291@users.noreply.github.com>
2025-10-04 18:07:13 +0100

    Added component that returns a UL of prop items

--- src/App.js
+++ src/App.js
@@ -3,9 +3,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <VariableDisplay/>
+      <OutputArray 
+        list = {[1,2,3]}
+        hello = "hi"
+        />
     </div>
   );
+}
+
+function VariableDisplay(){// eslint-disable-next-line
+    const str = "abc";// eslint-disable-next-line
+    const num = 0;// eslint-disable-next-line
+    const bool = true;// eslint-disable-next-line
+    const arr = ["a","b","c"];
+    const obj = {propOne:"a", propTwo:2, propThree:false};
+    return (obj.propOne);
+}
+
+function OutputArray(prop){
+  const listItems = prop.list.map((item) => <li key={item}>{item}</li>);
+  return (<ul className="list">{listItems}</ul>);
 }
 
 export default App;
