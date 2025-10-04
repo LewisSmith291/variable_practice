0bedc7c16dc1db54392cf7c59d238a27aa473e2f
Lewis Smith <66628534+PKThunda291@users.noreply.github.com>
2025-10-04 20:49:02 +0100

    Finished default task. Started making button that randomly changes the variable that is displayed.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
   return (
     <div className="App">
       <VariableDisplay/>
-      <OutputArray 
-        list = {[1,2,3]}
-        hello = "hi"
-        />
+      <br/>
+      <VariableButton/>
     </div>
   );
 }
 
-function VariableDisplay(){// eslint-disable-next-line
-    const str = "abc";// eslint-disable-next-line
-    const num = 0;// eslint-disable-next-line
-    const bool = true;// eslint-disable-next-line
-    const arr = ["a","b","c"];
-    const obj = {propOne:"a", propTwo:2, propThree:false};
-    return (obj.propOne);
+let varType = "array";
+
+let yessir = {varType: "array"};
+
+function VariableButton(){
+  return (<button onClick={setRandomVariable} id="button">Randomise Variable</button>)
+}
+
+function VariableDisplay(){
+  const str = "abc";
+  const num = 0;
+  const bool = true;
+  const arr = ["a","b","c"];
+  const obj = {propOne:"a", propTwo:2, propThree:false};
+  var objectToReturn;
+  
+  switch (varType){
+    case "string":
+      objectToReturn = str;
+      break;
+    case "number":
+      objectToReturn = num;
+      break;
+    case "boolean":
+      objectToReturn = bool;
+      break;
+    case "array":
+      objectToReturn = arr;
+      return (<OutputArray list = {arr}/>);
+    case "object":
+      objectToReturn = obj;
+      break;
+    default:
+      objectToReturn = str;
+      break;
+  }
+  console.log(objectToReturn);
+  return (objectToReturn);
+}
+
+
+function setRandomVariable(){
+  const typeList = ["string", "number", "boolean", "array", "object"];
+  const type = typeList[Math.floor(Math.random() * typeList.length)];
+  varType = type;
+  console.log(varType);
 }
 
 function OutputArray(prop){
