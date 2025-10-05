729ddb57af7c2b22ce08619057380392ad9579f8
Lewis Smith <66628534+PKThunda291@users.noreply.github.com>
2025-10-05 14:17:18 +0100

    Added button to randomly change the data type that is output

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import './App.css';
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 function App() {
   return (
     <div className="App">
       <VariableDisplay/>
-      <br/>
-      <VariableButton/>
     </div>
   );
 }
 
-let varType = "array";
-
-let yessir = {varType: "array"};
+var varType = "string";
 
 function VariableButton(){
   return (<button onClick={setRandomVariable} id="button">Randomise Variable</button>)
@@ -21,46 +17,68 @@ function VariableButton(){
 
 function VariableDisplay(){
   const str = "abc";
-  const num = 0;
+  const num = 123;
   const bool = true;
   const arr = ["a","b","c"];
-  const obj = {propOne:"a", propTwo:2, propThree:false};
-  var objectToReturn;
+  const obj = {propOne:"hello", propTwo:1231231, propThree:"this is an object"};
   
-  switch (varType){
-    case "string":
-      objectToReturn = str;
-      break;
-    case "number":
-      objectToReturn = num;
-      break;
-    case "boolean":
-      objectToReturn = bool;
-      break;
-    case "array":
-      objectToReturn = arr;
-      return (<OutputArray list = {arr}/>);
-    case "object":
-      objectToReturn = obj;
-      break;
-    default:
-      objectToReturn = str;
-      break;
+  var [output, setOutput] = useState("string");
+
+  useEffect(function() {
+    setInterval(function(){
+      switch (varType){
+      case "string":
+        setOutput(str);
+        break;
+      case "number":
+        setOutput(num);
+        break;
+      case "boolean":
+        setOutput(bool.toString());
+        break;
+      case "array":
+        setOutput(OutputArray(arr));
+        break;
+      case "object":
+        setOutput(OutputObject(obj));
+        break;
+      default:
+        setOutput(str);
+        break;
   }
-  console.log(objectToReturn);
-  return (objectToReturn);
+    },1);
+  });
+
+  return (
+    <div>
+      <VariableButton />
+      <br/>
+      {output}
+    </div>
+  );
 }
 
 
 function setRandomVariable(){
   const typeList = ["string", "number", "boolean", "array", "object"];
   const type = typeList[Math.floor(Math.random() * typeList.length)];
+
   varType = type;
-  console.log(varType);
 }
 
-function OutputArray(prop){
-  const listItems = prop.list.map((item) => <li key={item}>{item}</li>);
+function OutputObject(object){
+  
+  return (<div>
+    <ul>
+      <li key = "propOne">{object.propOne}</li>
+      <li key = "propTwo">{object.propTwo}</li>
+      <li key = "propThree">{object.propThree}</li>
+    </ul>
+  </div>)
+}
+
+function OutputArray(array){
+  const listItems = array.map((item) => <li key={item}>{item}</li>);
   return (<ul className="list">{listItems}</ul>);
 }
 
